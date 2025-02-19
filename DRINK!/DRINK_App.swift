27f1252976ee1b9d3ce096da3d8af8320fe0b0bf
Paulo Henrique Pianissola de Cerqueira <paulophpc@gmail.com>
2025-02-19 18:47:30 +0000

    Initial Commit

---
+++ DRINK!/DRINK_App.swift
@@ -0,0 +1,17 @@
+//
+//  DRINK_App.swift
+//  DRINK!
+//
+//  Created by Paulo Cerqueira on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct DRINK_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
